cba198778f456c8f453b842ba3bf88dbe47cb0f4
Dave Burchell <burchell@acm.org>
2024-12-06 08:32:25 -0600

    Python solution for part 2

---
+++ aoc20241204/part2.py
@@ -0,0 +1,142 @@
+#!/usr/local/bin/python3
+
+import fileinput
+import re
+
+def mas_cross_count(lines):
+	count = 0
+	i = 1
+	while i < len(lines) - 1:
+		j = 1
+		while j < len(lines[0]) - 1:
+			print("i: " + str(i) + "; j: " + str(j))
+			if mas_check(lines[i - 1][j - 1] + lines[i][j] + lines[i + 1][j + 1]) and mas_check(lines[i + 1][j - 1] + lines[i][j] + lines[i - 1][j + 1]):
+				count += 1
+			j += 1
+		i += 1
+	return count
+
+def mas_check(yomas):
+	print("Str to check: " + yomas)
+	if (yomas == "MAS" or yomas == "SAM"):
+		return True
+	return False
+
+def tally(lines):
+	matches = 0
+	matcharray = []
+	for line in lines:
+		matcharray = re.findall(r'XMAS', line)
+		matches = matches + len(matcharray)
+		matcharray = re.findall(r'SAMX', line)
+		matches = matches + len(matcharray)
+	return matches
+
+def matdump(lines):
+	for line in lines:
+		print(line)
+
+def rot90(matIn):
+	matOut = []
+	i = 0
+	while i < len(matIn[0]):
+		matOut.append("")
+		i += 1
+	j = 0
+	while j < len(matIn[0]):
+		i = len(matIn) - 1
+		while i >= 0:
+			matOut[j] = matOut[j] + matIn[i][j]
+			i -= 1
+		j += 1
+	return matOut
+
+# Rotate the puzzle space 45 degrees clockwise
+# The input must be a rectangular matrix
+def rot45(matIn):
+
+	# Initialize the array
+	matOut = []
+	i = 0
+	while i < len(matIn[0]) + len(matIn) - 1:
+		matOut.append("")
+		i += 1
+
+	# Handle the diagonals (going up and right) that originate
+	# from a first column position
+	i = 0
+	while i < len(matIn):
+		diag = 0
+		while i - diag >= 0 and diag < len(matIn[0]):
+			#print("i: " + str(i) + "; diag: " + str(diag) + "; Accessing char " + matIn[i - diag][diag])
+			matOut[i] = matOut[i] + matIn[i - diag][diag]
+			diag += 1
+		i += 1
+
+	# Handle the diagonals (going up and right) that originate
+	# from the bottom row starting with the second column
+	j = 1
+	while j < len(matIn[0]):
+		diag = 0
+		while j + diag < len(matIn[0]) and len(matIn) - 1 - diag >= 0:
+			character = matIn[len(matIn) - 1 - diag][j + diag]
+			#print("j: " + str(j) + "; diag: " + str(diag) + "; Accessing char " + character)
+			matOut[len(matIn) - 1 + j] = matOut[len(matIn) - 1 + j] + character
+			diag += 1
+		j += 1
+
+	return matOut
+
+tot = 0
+a = []
+for line in fileinput.input():
+	a.append(line.rstrip())
+
+a_alt = (
+'MMMSXXMASM',
+'MSAMXMSMSA',
+'AMXSXMAAMM',
+'MSAMASMSMX',
+'XMASAMXAMM',
+'XXAMMXXAMA',
+'SMSMSASXSS',
+'SAXAMASAAA',
+'MAMMMXMMMM',
+'MXMXAXMASX'
+)
+
+print("original")
+matdump(a)
+print("rot 45")
+matdump(rot45(a))
+
+tot += tally(a)
+tot += tally(rot45(a))
+tot += tally(rot90(a))
+tot += tally(rot45(rot90(rot90(rot90(a)))))
+print("rot 0: ")
+matdump(a)
+print("rot 45: ")
+matdump(rot45(a))
+print("rot 90: ")
+matdump(rot90(a))
+print("rot 180: ")
+matdump(rot90(rot90(a)))
+print("rot 270: ")
+matdump(rot90(rot90(rot90(a))))
+print("rot 315: ")
+matdump(rot45(rot90(rot90(rot90(a)))))
+
+testbed = []
+testbed = (
+	"abcde",
+	"ABCDE",
+	"12345")
+testbed2 = (
+	"abc",
+	"ABC",
+	"123",
+	"ijk",
+	"xyz")
+print("Tally: " + str(tot))
+print("MAS cross count: " + str(mas_cross_count(a)))
