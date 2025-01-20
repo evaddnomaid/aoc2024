6010ff8a2f08d7fc50b22a0f3f9c7396cbb463db
Dave Burchell <burchell@acm.org>
2025-01-19 21:21:48 -0600

    Further cleanup

--- aoc20241211/blinks.pl
+++ aoc20241211/blinks.pl
@@ -2,38 +2,33 @@
 use strict;
 
 sub counter {
+	my ($stone, $blinks) = @_;
 	our %cache;
-	my $stone = sprintf("%d", $_[0]);
-	my $blinks = $_[1];
-	if (defined($cache{$stone}[$blinks])) {
-		return $cache{$stone}[$blinks];
-	}
-	if ($blinks == 0) {
-		$cache{$stone}[$blinks] = 1;
-		return $cache{$stone}[$blinks];
-	}
-	if ($stone == 0) {
-		$cache{$stone}[$blinks] = counter(1, $blinks - 1);
-		return $cache{$stone}[$blinks];
-	}
-	my $stone_digits_count = length($stone);
-	if ($stone_digits_count % 2) {
-		my $next = $stone * 2024;
-		$cache{$stone}[$blinks] = counter($next, $blinks - 1);
-	} else {
-		my $left  = substr($stone, 0, $stone_digits_count / 2);
-		my $right = substr($stone, $stone_digits_count / 2);
-		$cache{$stone}[$blinks] = counter($left, $blinks - 1) + counter($right, $blinks - 1);
+	$stone = sprintf("%d", $stone);
+	if (!defined($cache{$stone}[$blinks])) {
+		if ($blinks == 0) {
+			$cache{$stone}[$blinks] = 1;
+		} elsif ($stone == 0) {
+			$cache{$stone}[$blinks] = counter(1, $blinks - 1);
+		} else {
+			if (length($stone) % 2) {
+				$cache{$stone}[$blinks] = counter($stone * 2024, $blinks - 1);
+			} else {
+				$cache{$stone}[$blinks]
+					= counter(substr($stone, 0, length($stone) / 2), $blinks - 1)
+					+ counter(substr($stone,  length($stone) / 2), $blinks - 1);
+			}
+		}
 	}
 	return $cache{$stone}[$blinks];
 }
 
-my $blinks = 75;
+my $blinkings = 75;
 my @stonelist = qw(4189 413 82070 61 655813 7478611 0 8);
 my $tot = 0;
 
 foreach ( @stonelist ) {
 	print $_ . "\n";
-	$tot += counter($_, $blinks);
+	$tot += counter($_, $blinkings);
 	print "Running total: " . $tot . "\n";
 }
